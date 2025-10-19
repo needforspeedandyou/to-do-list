19d4550b07958838935030af5344673d258f2a57
VADIM PURHIN <purginvadim8@gmail.com>
2025-10-19 20:21:15 +0300

    Clean up script.js by removing unused code
    Removed commented-out code for task management buttons and event listeners.

--- script.js
+++ script.js
@@ -20,11 +20,6 @@ function renderTasks() {
         deleteButton.innerHTML = "Delete"
         buttonContainer.appendChild(deleteButton)
 
-        // const doneButton = document.createElement("button")
-        // doneButton.setAttribute("class", "green")
-        // doneButton.innerHTML = "Done"
-        // buttonContainer.appendChild(doneButton)
-
         deleteButton.addEventListener("click", () => deleteTask(index))
     });
 }
@@ -56,46 +51,3 @@ function doneTask(index) {
 addButton.addEventListener("click", () => addTask())
 
 renderTasks()
-
-// addButton.addEventListener("click", () => {
-//     const taskName = document.getElementById("taskText").value
-//     console.log(tasks)
-
-//     const task = document.createElement("li")
-//     task.setAttribute("class", "task")
-//     task.innerHTML = `<p>${taskName}</p><span><button id='deleteButton' class='red'>Delete</button><button id='doneButton' class='green'>Done</button></span>`
-//     taskList.appendChild(task)
-
-//     const deleteButton = document.getElementById("deleteButton")
-//     deleteButton.addEventListener("click", () => {
-//         var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-//         localStorage.setItem('tasks', JSON.stringify(tasks))
-
-
-//     })
-
-//     tasks.push(taskName)
-//     localStorage.setItem("tasks", JSON.stringify(tasks))
-
-//     console.log(localStorage.getItem("tasks"))
-// })
-
-// window.addEventListener("DOMContentLoaded", () => {
-//     var tasks = JSON.parse(localStorage.getItem("tasks")) || []
-
-//     tasks.forEach(element => {
-//         const task = document.createElement("li")
-//         task.setAttribute("class", "task")
-//         task.innerHTML = `<p>${element}</p><span><button id='deleteButton' class='red'>Delete</button><button id='doneButton' class='green'>Done</button></span>`
-//         taskList.appendChild(task)
-
-//         const deleteButton = document.getElementById("deleteButton")
-//         deleteButton.addEventListener("click", () => {
-//             var taskID = tasks.indexOf(element)
-//             tasks.splice(taskID, 1)
-//             localStorage.setItem('tasks', JSON.stringify(tasks))
-//             console.log("asdasd")
-//         })
-//     });
-// })
